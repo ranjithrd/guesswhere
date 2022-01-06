dd3e65421df5c1b056007df6e5b8137f9a7b4d59
Ranjith RD <ranjnithrd03@gmail.com>
2022-01-06 18:57:51 +0530

    Added create func

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { useHistory } from "react-router-dom"
 import { GameService } from "../Service"
+import Fingerprint from "@fingerprintjs/fingerprintjs"
 
 function Home() {
 	const history = useHistory()
@@ -9,6 +10,9 @@ function Home() {
 	const [error, setError] = useState()
 
 	async function handleCode() {
+		if (code < 100000 || code > 1000000) {
+			setError("Invalid code.")
+		}
 		setError("Loading...")
 		const existingGame = await GameService.findGameWithCode(code)
 		if (existingGame) {
@@ -18,7 +22,29 @@ function Home() {
 		}
 	}
 
-	async function handleCreate() { }
+	async function handleCreate() {
+		let randomNumber = 0
+		while (
+			randomNumber < 100000 ||
+			randomNumber > 1000000 ||
+			(await GameService.findGameWithCode(randomNumber)) !== null
+		) {
+			randomNumber = Math.ceil(Math.random() * 1000000)
+		}
+		const fp = await Fingerprint.load()
+		const id = (await fp.get()).visitorId
+
+		await GameService.updateGame(randomNumber, {
+			code: randomNumber,
+			expires: "2022-0-0",
+			lastAnswerer: id,
+			lastQuestioner: id,
+			players: JSON.stringify([id]),
+			started: false,
+		})
+
+		history.push(`/game/${randomNumber}`)
+	}
 
 	return (
 		<div className="h-full w-full p-16 flex flex-col justify-between">
@@ -33,10 +59,14 @@ function Home() {
 					placeholder="Enter a code"
 				/>
 				<div className="h-4"></div>
-				<button onClick={handleCode} className="filled">Enter</button>
+				<button onClick={handleCode} className="filled">
+					Enter
+				</button>
 				<p className="text-red-500">{error}</p>
 			</div>
-			<button onClick={handleCreate} className="filled">Create a Game</button>
+			<button onClick={handleCreate} className="filled">
+				Create a Game
+			</button>
 		</div>
 	)
 }
